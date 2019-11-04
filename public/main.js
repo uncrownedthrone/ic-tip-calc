56ed0764328d0e2963177ef7965a196a3d3880d3
Justin Swiger <justin.swiger@me.com>
2019-11-04 11:46:37 -0500

    added js

--- public/main.js
+++ public/main.js
@@ -1,7 +1,24 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  console.log('hello world')
+}
+
+const calculateTip = () => {
+  console.log('calculating tip')
+  // get bill total from the input tag
+  const total = document.querySelector('input').value
+  console.log('the total is ' + total)
+  // calculate the different tips
+  const fifteenPercentTip = total * 0.15
+  const eighteenPercentTip = total * 0.18
+  const twentyPercentTip = total * 0.2
+  console.log(fifteenPercentTip)
+  console.log(eighteenPercentTip)
+  console.log(twentyPercentTip)
+  // display the tip amounts
+  document.querySelector('.fifteen-percent').textContent = fifteenPercentTip
+  document.querySelector('.eighteen-percent').textContent = eighteenPercentTip
+  document.querySelector('.twenty-percent').textContent = twentyPercentTip
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('button').addEventListener('click', calculateTip)
